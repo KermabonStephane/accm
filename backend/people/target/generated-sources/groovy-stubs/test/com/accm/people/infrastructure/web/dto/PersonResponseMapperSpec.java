5d1b3fd7069d42bb6a7365169c21e4b250dc0cec
Kermabon Stéphane <demis27@demis27.net>
2026-04-15 14:52:44 +0200

    chore: untrack backend/people/target build artifacts

--- backend/people/target/generated-sources/groovy-stubs/test/com/accm/people/infrastructure/web/dto/PersonResponseMapperSpec.java
+++
@@ -1,9 +0,0 @@
-package com.accm.people.infrastructure.web.dto;
-
-public class PersonResponseMapperSpec
-  extends spock.lang.Specification  implements
-    groovy.lang.GroovyObject {
-;
-@groovy.transform.Generated() @groovy.transform.Internal() @java.beans.Transient() public  groovy.lang.MetaClass getMetaClass() { return null; }
-@groovy.transform.Generated() @groovy.transform.Internal() public  void setMetaClass(groovy.lang.MetaClass mc) { }
-}
